9b2d9039531776a391ef0dbed65bbeacc94d42b0
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-06-11 18:52:42 -0400

    get context working by installing jest-context-plugin

--- src/__tests__/checkbox_with_label_test.js
+++ src/__tests__/checkbox_with_label_test.js
@@ -7,6 +7,7 @@ import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import * as TestUtils from 'react-dom/test-utils';
 import CheckboxWithLabel from '../CheckboxWithLabel';
+import context from 'jest-plugin-context';
 
 it('renders correctly', () => {
     const tree = renderer
@@ -14,21 +15,25 @@ it('renders correctly', () => {
       .toJSON();
     expect(tree).toMatchSnapshot();
 });
+describe('a sane universe', () => {
+  context('planet earth', () => {
 
-it('CheckboxWithLabel changes the text after click', () => {
-  // Render a checkbox with label in the document
-  const checkbox = TestUtils.renderIntoDocument(
-    <CheckboxWithLabel labelOn="On" labelOff="Off" />
-  );
+    it('CheckboxWithLabel changes the text after click', () => {
+      // Render a checkbox with label in the document
+      const checkbox = TestUtils.renderIntoDocument(
+        <CheckboxWithLabel labelOn="On" labelOff="Off" />
+      );
 
-  const checkboxNode = ReactDOM.findDOMNode(checkbox);
+      const checkboxNode = ReactDOM.findDOMNode(checkbox);
 
-  // Verify that it's Off by default
-  expect(checkboxNode.textContent).toEqual('Off');
+      // Verify that it's Off by default
+      expect(checkboxNode.textContent).toEqual('Off');
 
-  // Simulate a click and verify that it is now On
-  TestUtils.Simulate.change(
-    TestUtils.findRenderedDOMComponentWithTag(checkbox, 'input')
-  );
-  expect(checkboxNode.textContent).toEqual('On');
-});
+      // Simulate a click and verify that it is now On
+      TestUtils.Simulate.change(
+        TestUtils.findRenderedDOMComponentWithTag(checkbox, 'input')
+      );
+      expect(checkboxNode.textContent).toEqual('On');
+    });
+  })
+})
