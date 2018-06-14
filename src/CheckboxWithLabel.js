49cd253362fa389d6be827abe668cb7b0d1f4016
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-06-14 18:25:51 -0400

    get things to play nice.

--- src/CheckboxWithLabel.js
+++ src/CheckboxWithLabel.js
@@ -6,7 +6,7 @@ import React from 'react';
 export default class CheckboxWithLabel extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {isChecked: false};
+    this.state = {isChecked: false, count:0};
 
     // bind manually because React class components don't auto-bind
     // http://facebook.github.io/react/blog/2015/01/27/react-v0.13.0-beta-1.html#autobinding
@@ -15,6 +15,7 @@ export default class CheckboxWithLabel extends React.Component {
 
   onChange() {
     this.setState({isChecked: !this.state.isChecked});
+    this.setState({count: this.state.count+1})
   }
 
   render() {
@@ -26,6 +27,7 @@ export default class CheckboxWithLabel extends React.Component {
           onChange={this.onChange}
         />
         {this.state.isChecked ? this.props.labelOn : this.props.labelOff}
+        {(this.state.count > 4) ? <a href="calc.html">click here</a> :<p>click the checkbox a few times</p>}
       </label>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CheckboxWithLabel from './CheckboxWithLabel';
 import registerServiceWorker from './registerServiceWorker';
+import Renderer from './Renderer';
 
 ReactDOM.render(<Renderer />, document.getElementById('root'));
 registerServiceWorker();
